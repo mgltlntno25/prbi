37ac828924a4bd0447a3c9a7693726e949cfee41
Juan Miguel <jntolentino@fit.edu.ph>
2019-08-29 07:18:18 +0800

    email4

--- resources/views/mail_verify.blade.php
+++ resources/views/mail_verify.blade.php
@@ -9,7 +9,7 @@
 <form action="{{url('/user/emailVerify)}}" method="post">
     {{ csrf_field() }}
 
-        <input type="hidden" class="form-control" name="email" placeholder="Reference Number" value="{{$data['email']}}">
+        <input type="hidden" class="form-control" name="email"  value="{{$data['email']}}">
     
     <p align="center"><button type="submit" class="btn btn-success mb-2"><i class="fa fa-bank"></i> Verify Email</button>
     </p>
