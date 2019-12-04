39200341e20d824431379b379ba6b0727bca49d5
Juan Miguel <jntolentino@fit.edu.ph>
2019-12-05 05:13:46 +0800

    clean2

--- resources/views/admin/admin_includes/header_navbar.blade.php
+++ resources/views/admin/admin_includes/header_navbar.blade.php
@@ -56,7 +56,7 @@
           </li>
           <!-- Control Sidebar Toggle Button -->
           <li>
-            <a href="#" data-toggle="control-sidebar"><i class="fa fa-bell"></i></a>
+            
           </li>
         </ul>
       </div>
